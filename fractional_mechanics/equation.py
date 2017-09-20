c4ec3d27d062c6c2b6534c97fcd283acdca36766
szajek <krzysztof.szajek.mail@gmail.com>
2017-09-20 15:59:03 +0200

    Introduce dynamic value for precision of fractional derivative approximation.

--- fractional_mechanics/equation.py
+++ fractional_mechanics/equation.py
@@ -1,18 +1,20 @@
 from fractulus.equation import create_riesz_caputo_stencil
-from fdm import Operator, Number, Stencil
+from fdm import Operator, Number, Stencil, LocalizedStencil
 
 
 __all__ = ['create_fractional_deformation_operator']
 
 
-def create_fractional_deformation_operator(settings):
+def create_fractional_deformation_operator(settings, dynamic_resolution=lambda node_address: 1.):
     alpha = settings.alpha
     p = settings.resolution
 
     multiplier = p ** (alpha - 1)  # l_ef**(alpha-1) = p**(alpha-1) * 1./h**(1-alpha) <- lef describes by grid span
 
+    riesz_caputo_stencil = create_riesz_caputo_stencil(settings, increase_order_by=1 - alpha, dynamic_resolution=dynamic_resolution)
+
     return Operator(
-        Number(multiplier) * create_riesz_caputo_stencil(settings, increase_order_by=1-alpha),
+        Number(multiplier) * riesz_caputo_stencil,
         Operator(  # todo: replace with Operator.order(n)
             Stencil.central(1.),
         )
