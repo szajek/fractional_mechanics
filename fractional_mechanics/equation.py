7684477906218d3d32acc72fd238f418b1072055
szajek <krzysztof.szajek.mail@gmail.com>
2017-10-16 10:28:27 +0200

    Allow to vary resolution of Caputo's derivative approximation.

--- fractional_mechanics/equation.py
+++ fractional_mechanics/equation.py
@@ -5,17 +5,19 @@ from fdm import Operator, Number, Stencil, LocalizedStencil
 __all__ = ['create_fractional_deformation_operator']
 
 
-def create_fractional_deformation_operator(settings, dynamic_resolution=lambda node_address: 1.):
+def create_fractional_deformation_operator(settings, dynamic_resolution=lambda node_address: 1., stencil=None):
     alpha = settings.alpha
-    p = settings.resolution
+    # p = settings.resolution
 
-    multiplier = p ** (alpha - 1)  # l_ef**(alpha-1) = p**(alpha-1) * 1./h**(1-alpha) <- lef describes by grid span
+    def multiplier(address):
+        return dynamic_resolution(address) ** (alpha - 1)  # l_ef**(alpha-1) = p**(alpha-1) * 1./h**(1-alpha) <- lef describes by grid span
 
+    # riesz_caputo_stencil = create_riesz_caputo_stencil(settings, dynamic_resolution=dynamic_resolution)
     riesz_caputo_stencil = create_riesz_caputo_stencil(settings, increase_order_by=1 - alpha, dynamic_resolution=dynamic_resolution)
 
     return Operator(
         Number(multiplier) * riesz_caputo_stencil,
         Operator(  # todo: replace with Operator.order(n)
-            Stencil.central(1.),
+            stencil or Stencil.central(1.),
         )
     )
